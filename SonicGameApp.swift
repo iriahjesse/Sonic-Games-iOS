1cf6fe1b2af0d0858bfac2019909e31b6cfcb1ce
Jesse Iriah <iriahjesse@gmail.com>
2025-09-25 14:23:13 -0400

    Create SonicGameApp.swift

---
+++ SonicGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonicGamesApp.swift
+//  SonicGames
+//
+//  Created by Jesse Iriah on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SonicGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
